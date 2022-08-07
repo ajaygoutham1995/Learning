ba06f31dea020c5c695860a3cd8bfe22f10c4c2c
ajaygoutham1995 <48481038+ajaygoutham1995@users.noreply.github.com>
2022-08-07 13:59:00 +0530

    Revert "Changed Few lines in app.java"
    This reverts commit 1cf2a761d98489016564647fbff6d513f10a1e00.

--- Learning/src/main/java/com/learn/App.java
+++ Learning/src/main/java/com/learn/App.java
@@ -15,5 +15,6 @@ public class App
         System.out.println( "added line again" );
         System.out.println( "Checking" );
         System.out.println( "added master line" );
+        System.out.println( "Sample" );
     }
 }
--- Learning/src/main/java/com/learn/app2.java
+++ Learning/src/main/java/com/learn/app2.java
@@ -4,9 +4,8 @@ public class app2 {
     public static void main( String[] args )
     {
 
-        System.out.println( "app1" );
+        System.out.println( "app2" );
         System.out.println( "Sample app2" );
-
 
     }
 }
